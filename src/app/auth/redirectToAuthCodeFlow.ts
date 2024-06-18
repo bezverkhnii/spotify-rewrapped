ea552cdf4566773e313abcc927e52b3a7b0bafd6
Rustam Liaukouski <rustam.liaukouski@careguideplatform.onmicrosoft.com>
2024-06-18 23:55:04 +0200

    added prod uri

--- src/app/auth/redirectToAuthCodeFlow.ts
+++ src/app/auth/redirectToAuthCodeFlow.ts
@@ -10,7 +10,7 @@ export async function redirectToAuthCodeFlow(clientId: string) {
   const params = new URLSearchParams();
   params.append("client_id", clientId);
   params.append("response_type", "code");
-  params.append("redirect_uri", "http://localhost:3000");
+  params.append("redirect_uri", "https://spotify-rewrapped-nu.vercel.app");
   params.append("scope", "user-read-private user-read-email user-top-read");
   params.append("code_challenge_method", "S256");
   params.append("code_challenge", challenge);
